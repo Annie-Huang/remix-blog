725fa435458fdebc839c730f1d83659d8a241844
Annie Huang <annie.hn@gmail.com>
2021-12-25 22:22:11 +1100

    Mimic making a api call in server side and get the data from the frontend.

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,13 +1,23 @@
 import { useLoaderData } from 'remix';
 
+// Mimic making a api call in server side and get the data from the frontend.
 export const loader = () => {
   // loader runs in the server, you can see it log in terminal, not chrome devtool
-  console.log('123');
+  // console.log('123');
 
-  return {};
+  const data = {
+    posts: [
+      { id: 1, title: 'Post 1', body: 'This is a test post' },
+      { id: 2, title: 'Post 2', body: 'This is a test post' },
+      { id: 3, title: 'Post 3', body: 'This is a test post' },
+    ],
+  };
+  return data;
 };
 
 const PostItems = () => {
+  const { posts } = useLoaderData();
+
   return (
     <div>
       <h1>Posts</h1>
