3da80f41d6d8dfb4dc77c6ca6a746b51b91e7de2
Annie Huang <annie.hn@gmail.com>
2021-12-26 16:40:57 +1100

    Create loader in app/routes/posts/$postId.jsx to get the post item from the DB.

--- app/routes/posts/$postId.jsx
+++ app/routes/posts/$postId.jsx
@@ -1,11 +1,25 @@
-import { useParams } from 'remix';
+import { useParams, useLoaderData } from 'remix';
+import { db } from '~/utils/db.server';
+
+export const loader = async ({ params }) => {
+  const post = await db.post.findUnique({
+    where: { id: params.postId },
+  });
+
+  if (!post) throw new Error('Post not found');
+
+  const data = { post };
+  return data;
+};
 
 const Post = () => {
-  const params = useParams();
+  // const params = useParams();
+  const { post } = useLoaderData();
 
   return (
     <div>
-      <h1>Post {params.postId}</h1>
+      {/*<h1>Post {params.postId}</h1>*/}
+      <h1>{post.title}</h1>
     </div>
   );
 };
