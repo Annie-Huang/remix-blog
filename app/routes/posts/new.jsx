c555bcb6f78b1400f7424a7fd8b04b0345bc83d7
Annie Huang <annie.hn@gmail.com>
2021-12-26 13:13:16 +1100

    Create the NewPost form

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,8 +1,30 @@
+import { Link } from 'remix';
+
 const NewPost = () => {
   return (
-    <div>
-      <h1>New Post</h1>
-    </div>
+    <>
+      <div className='page-header'>
+        <h1>New Post</h1>
+        <Link to='/posts' className='btn btn-reverse'>
+          Back
+        </Link>
+      </div>
+      <div className='page-content'>
+        <form action='' method='post'>
+          <div className='form-control'>
+            <label htmlFor='title'>Title</label>
+            <input type='text' name='title' id='title' />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='body'>Post Body</label>
+            <textarea name='body' id='body' />
+          </div>
+          <button type='submit' className='btn btn-block'>
+            Add Post
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
